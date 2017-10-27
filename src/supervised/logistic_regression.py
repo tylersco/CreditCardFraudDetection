e30b6c435124f9bc8d467f410825a1c9ad0b6940
Nicl7004 <nicholas.clement@colorado.edu>
2017-10-27 15:42:29 -0600

    accuracy score of 0.9992

--- src/supervised/logistic_regression.py
+++ src/supervised/logistic_regression.py
@@ -5,29 +5,49 @@ from sklearn import linear_model
 
 class LogisticRegression:
 
-    def logreg(self):
-        path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-        path += '/data/creditcard.csv'
+    def logreg(self, X, y):
 
-        df = pd.read_csv(path)
-
-        X = df.drop("Class", axis=1).drop("Time", axis=1)
+        results = {}
 
         # According to online sources, LogisticRegression can handle multiple classes ootb
         log_reg_model = linear_model.LogisticRegression()
 
-        log_reg_model.fit(X, df["Class"])
+        log_reg_model.fit(X, y)
 
         results = log_reg_model.predict(X)
 
-        for output in results:
-            if output != 0:
-                print(output)
+        accuracy = log_reg_model.score(X,y)
+
+        # for output in results:
+        #     if output != 0:
+        #         print(output)
+
+        return (results, accuracy)
+
+
+
 
 
 def main():
+    # Create dataframe
+    path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    path += '/data/creditcard.csv'
+    df = pd.read_csv(path)
+
+    # X and Y used for sklearn logreg
+    X = df.drop("Class", axis=1).drop("Time", axis=1)
+    y = df["Class"]
+
     logistic_regression = LogisticRegression()
-    logistic_regression.logreg()
+
+    total = logistic_regression.logreg(X,y)
+    print(total[0])
+    print(total[1])
+
+    # print("Number of positives = " + str(len(results)))
+    # print(logistic_regression.accuracy())
+
+
 
 if __name__ == '__main__':
     main()
