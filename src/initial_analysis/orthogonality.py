29c46fe49661a70de6812063665e3c69d1154ca5
tylersco <tylersco4@gmail.com>
2017-10-27 19:40:32 -0600

    Update neural net

--- src/initial_analysis/orthogonality.py
+++ src/initial_analysis/orthogonality.py
@@ -1,13 +1,14 @@
-
 import pandas as pd
 import numpy as np
 import os
 
-path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-path += '/data/creditcard.csv'
-
 df = pd.read_csv(path,sep=",")
-df1 = df[1:28]
+df1 = df[2:27]
+
+res = []
+
+for i in range(2, 28):
+    res.append(np.dot(df1[i], df1))
 product = np.dot(df1,df1.T)
 # print("Vector1: {0} , Vector2:{1}".format(vector,vector1))
 print(product)
