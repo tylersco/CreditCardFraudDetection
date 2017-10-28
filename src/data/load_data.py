8f6cfeb1f8286e7ba7aa8da4f84a8391143be429
tylersco <tylersco4@gmail.com>
2017-10-28 14:49:10 -0600

    Update neural network to use reduced dataset

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-def load_data(path):
+def load_data(path, skip_columns=None):
     """
     Load the credit card fraud data
 
     path: Absolute path to the creditcard.csv file
+    skip_columns: List of column indices (indexed by 0) to exclude in data
     returns: Four numpy arrays corresponding to the features and labels for genuine
              transactions and features and labels for fraudulent transactions
     """
@@ -19,5 +20,10 @@ def load_data(path):
     y_genuine = np.reshape(genuine[:, -1], (-1, 1))
     x_fraudulent = fraudulent[:, :-1]
     y_fraudulent = np.reshape(fraudulent[:, -1], (-1, 1))
+
+    if skip_columns:
+        keep_columns = [i for i in range(np.shape(x_genuine)[1]) if i not in skip_columns]
+        x_genuine = x_genuine[:, keep_columns]
+        x_fraudulent = x_fraudulent[:, keep_columns]
 
     return x_genuine, y_genuine, x_fraudulent, y_fraudulent
