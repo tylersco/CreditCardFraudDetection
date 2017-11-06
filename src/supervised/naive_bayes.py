b044a8732efc5a687b32978b49c930c3ca607884
Chris Struckman <chst7928@colorado.edu>
2017-11-06 00:14:50 -0700

    Added test and training sets to naive bayes rather than test the entire df

--- src/supervised/naive_bayes.py
+++ src/supervised/naive_bayes.py
@@ -6,16 +6,17 @@ import numpy as np
 sys.path.append('../data/')
 from sklearn.naive_bayes import GaussianNB as GNB
 from sklearn.metrics import confusion_matrix as conf
+from sklearn.model_selection import train_test_split
 
 
-def naive_bayes(X,Y):
+def naive_bayes(x_train,x_test,y_train,y_test):
 
     test = GNB();
-    test.fit(X,Y)
-    result = test.predict(X)
+    test.fit(x_train,y_train)
+    result = test.predict(x_test)
     print result
-    print test.score(X,Y)
-    print conf(Y,result)
+    print test.score(x_test, y_test)
+    print conf(y_test, result)
 
 path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 path += '/data/creditcard.csv'
@@ -23,6 +24,8 @@ df = pd.read_csv(path)
 X = df.drop("Class",axis=1).drop("Time",axis=1)
 Y = df["Class"]
 df = df.values;
+x_train, x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2)
 
-naive_bayes(X,Y)
+naive_bayes(x_train,x_test,y_train,y_test)
+
 
