f51bf939e2ab4cfe7dd93fdae92a334e47aa4459
Nicl7004 <nicholas.clement@colorado.edu>
2017-11-05 23:22:17 -0700

    added class weights to logistic regression

--- src/supervised/logistic_regression.py
+++ src/supervised/logistic_regression.py
@@ -7,8 +7,11 @@ class LogisticRegression:
 
     def logreg(self, X, y, test):
 
+        # Associate higher weight (of 2) with the positive class:
+        weights = {1: 2, 0: 1}
+
         # According to online sources, LogisticRegression can handle multiple classes ootb
-        log_reg_model = linear_model.LogisticRegression()
+        log_reg_model = linear_model.LogisticRegression(class_weight=weights)
 
         log_reg_model.fit(X, y)
 
@@ -36,15 +39,12 @@ def main():
     X = train.drop("Class", axis=1).drop("Time", axis=1)
     y = train["Class"]
 
-
     logistic_regression = LogisticRegression()
 
     total = logistic_regression.logreg(X, y, test)
     print(total[0])
     print(total[1])
     print(total[2])
-
-
 
 
 if __name__ == '__main__':
