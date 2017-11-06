54f689ce17ce7ab20e676cd33e6b19bbe74e7d87
Chris Struckman <chst7928@colorado.edu>
2017-11-05 23:34:57 -0700

    Apperance change of Naive Bayes

--- src/supervised/naive_bayes.py
+++ src/supervised/naive_bayes.py
@@ -8,21 +8,21 @@ from sklearn.naive_bayes import GaussianNB as GNB
 from sklearn.metrics import confusion_matrix as conf
 
 
-# def naive_bayes()
+def naive_bayes(X,Y):
+
+    test = GNB();
+    test.fit(X,Y)
+    result = test.predict(X)
+    print result
+    print test.score(X,Y)
+    print conf(Y,result)
 
 path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 path += '/data/creditcard.csv'
-
 df = pd.read_csv(path)
 X = df.drop("Class",axis=1).drop("Time",axis=1)
 Y = df["Class"]
-
 df = df.values;
 
-test = GNB();
-test.fit(X,Y)
-result = test.predict(X)
-print result
-print test.score(X,Y)
-print conf(Y,result)
+naive_bayes(X,Y)
 
